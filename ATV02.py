4980e1235cb6c6cc15ec3c7baeb7924b30da3b76
Kadu <161746694+CarlosSilva2007@users.noreply.github.com>
2025-04-07 11:57:23 +0000

    ATV02.py
    Atividade 02 - Funções. Atribuída no dia 28 de março, para o dia 20 de abril.

---
+++ ATV02.py
@@ -0,0 +1,19 @@
+#1️. Gerenciador de Tarefas Melhorado
+
+
+
+
+#2. Controle de Estoque Inteligente
+
+
+
+#3. Sistema de Reservas de Eventos
+
+
+
+#4. Sistema Bancário com Login
+
+
+
+#5. Gerenciador de Contatos
+
